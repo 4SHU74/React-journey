c9bbf6ce0624fd3560c1d5a496fa117fbb2a9677
iamashura <hackerz55org@gmail.com>
2025-11-04 11:27:02 +0100

    first ever work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Head from "./Head.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Head />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Head.jsx
@@ -0,0 +1,23 @@
+function Head() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Sections</a>
+          </li>
+          <li>
+            <a href="#">Games</a>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Head;
